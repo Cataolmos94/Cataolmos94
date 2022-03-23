a58edfbe81f657b54bf59aae6279425463e9fc98
Cataolmos94 <k_talina101@hotmail.com>
2022-03-22 20:44:10 -0500

    hola

---
+++ js/scriot.js
@@ -0,0 +1,76 @@
+$(".bullying1").css("color", "red")
+
+$(".bullying1").dblclick(function(){
+    $(".bullying1").css("background", "brown")
+})
+
+/*.clase*/
+
+$(".bullying2").click(function(){
+
+$(".bullying2").css("font-size", "30px").css("color","blue")
+})
+
+$(".bullying2").dblclick(function(){
+    $(".bullying2").css("background", "silver")
+})
+
+$(".evento2").dblclick(function(){
+
+    $(".evento2").css("font-size", "10px")
+    })
+
+/*bullying3, doble click fondo verde dblclik */
+
+$(".bullying3").dblclick(function(){
+    $(".bullying3").css("background", "purple")
+})
+
+$(".bullying4").dblclick(function(){
+    $(".bullying4").css("background", "violet")
+})
+
+$(".bullying5").dblclick(function(){
+    $(".bullying5").css("background", "green")
+})
+
+$(".bullying6").dblclick(function(){
+    $(".bullying6").css("background", "red")
+})
+
+$(".bullying7").dblclick(function(){
+    $(".bullying7").css("background", "yellow")
+})
+
+$(".bullying8").dblclick(function(){
+    $(".bullying8").css("background", "orange")
+})
+
+$(".bullying9").dblclick(function(){
+    $(".bullying9").css("background", "gray")
+})
+
+$(".bullying10").dblclick(function(){
+    $(".bullying10").css("background", "blue")
+})
+
+
+
+$(".caja1").mouseover(function(){
+
+    $(".caja").css("background", "red")
+
+})
+
+/* salga del parrafo, se vuelva fondo azul mouseout*/
+$(".caja1").mouseout(function(){
+    $(".caja1").css("background", "violet")
+})
+
+$(".caja2").mouseout(function(){
+    $(".caja2").css("background", "violet")
+})
+
+$(".caja3").mouseout(function(){
+    $(".caja3").css("background", "violet")
+})
